baa385e5e7e33fc6db7251df747c69cc5f0ea45e
CasinoRoyal <lux_ex_tenebris@outlook.com>
2021-10-13 16:16:20 +0300

    Create http port and its implementetion http adapter

--- front/src/servicies/http/http.adapter.ts
+++ front/src/servicies/http/http.adapter.ts
@@ -1,32 +1,52 @@
-import { Http } from './http';
-
-// TODO: REFACTOR HTTP METHODS
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+import { Http, HttpResponse } from './http';
 
 class HttpAdapter implements Http {
-  constructor(private readonly _baseUrl: string) {}
+  private readonly _http: AxiosInstance;
 
-  async getResourse<T>(endPoint: string): Promise<T> {
-    const response = await fetch(`${this._baseUrl}/${endPoint}`);
-
-    if (!response.ok) {
-      throw new Error('Something went wrong');
-    }
-
-    const data = response.json();
-
-    return data;
+  constructor(private readonly _baseUrl: string) {
+    this._http = axios.create({ baseURL: this._baseUrl });
   }
 
-  async pushResourse<T, B>(endPoint: string, options: B): Promise<T> {
-    const response = await fetch(`${this._baseUrl}/${endPoint}`, options);
+  async get<T>(endPoint: string): Promise<HttpResponse<T>> {
+    try {
+      const response: AxiosResponse<T> = await this._http.get(`${endPoint}`);
 
-    if (!response.ok) {
+      if (response.status !== 200) {
+        throw new Error('Something went wrong');
+      }
+
+      return response;
+    } catch (e) {
       throw new Error('Something went wrong');
     }
+  }
 
-    const data = response.json()
+  async post<Opt, TRes>(
+    endPoint: string,
+    options: Opt,
+  ): Promise<HttpResponse<TRes>> {
+    try {
+      const response = await this._http.post(`${endPoint}`, options);
 
-    return data;
+      if (response.status !== 200) {
+        throw new Error('Something went wrong');
+      }
+
+      return response;
+    } catch (e) {
+      throw new Error('Something went wrong');
+    }
+  }
+
+  async delete(endPoint: string): Promise<boolean> {
+    try {
+      const response = await this._http.delete(`${endPoint}`);
+
+      return (response.status === 200);
+    } catch (e) {
+      throw new Error('Something went wrong');
+    }
   }
 }
 
--- front/src/servicies/http/http.ts
+++ front/src/servicies/http/http.ts
@@ -1,11 +1,22 @@
-type HttpMethod = 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE';
-
-export type Options<B> = {
-  method: HttpMethod;
-  body: B;
+//type HttpMethod = 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE';
+type HttpBody<T> = Record<string, T>;
+type HttpOptions<B> = {
+  body: HttpBody<B>;
+  [key: string]: any;
+};
+export type HttpResponse<T> = {
+  data: T;
+  status: number;
+  statusText: string;
+  headers: any;
+  [key: string]: any;
 };
 
 export interface Http {
-  getResourse<T>(endPoint: string): Promise<T>;
-  pushResourse<T, B>(endPoint: string, options: Options<B>): Promise<T>;
+  get<T>(endPoint: string): Promise<HttpResponse<T>>;
+  post<Opt, TRes>(
+    endPoint: string,
+    options: HttpOptions<Opt>,
+  ): Promise<HttpResponse<TRes>>;
+  delete(endPoint: string): Promise<boolean>;
 }
