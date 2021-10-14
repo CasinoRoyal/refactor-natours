760eeb0e90d3e8962aee86e1f61c1dd969006869
CasinoRoyal <lux_ex_tenebris@outlook.com>
2021-10-14 17:47:18 +0300

    Add contain method which check is an element in the cart

--- front/src/domains/cart.entity.ts
+++ front/src/domains/cart.entity.ts
@@ -10,15 +10,21 @@ export class Cart {
     return this._items;
   }
 
-  addToCart(items: CartItems) {
+  addToCart(items: CartItems): Cart {
     return new Cart([...this._items, ...items]);
   }
 
-  removeFromCart(itemId: string) {
+  removeFromCart(itemId: string): CartItems {
     return this._items.filter(({ id }) => id !== itemId);
   }
 
   clearCart(): Cart {
     return new Cart([])
+  }
+
+  contains(itemId: string): boolean {
+    const result = this._items.find(({ id }) => id === itemId);
+
+    return !!result;
   }
 }
