792e15ffc7bd02edf1af06129ce618d24c108af5
CasinoRoyal <lux_ex_tenebris@outlook.com>
2021-11-06 20:03:30 +0200

    Add welcome page.

---
+++ front/src/ui/components/welcome.tsx
@@ -0,0 +1,22 @@
+import { ReactElement } from 'react';
+
+export function Welcome(): ReactElement {
+  return (
+    <div className="welcome">
+      <h2 className="heading-secondary">Welcome to Natours!</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur, adipisicing elit. Error
+        cupiditate aspernatur saepe dolor fugit veritatis corrupti laudantium
+        est quas velit, earum quaerat sed dolorem ullam voluptatum similique
+        laborum odit voluptates.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur, adipisicing elit. Error
+        cupiditate aspernatur saepe dolor fugit veritatis corrupti laudantium
+        est quas velit, earum quaerat sed dolorem ullam voluptatum similique
+        laborum odit voluptates.
+      </p>
+    </div>
+  );
+}
--- front/src/ui/pages/home.tsx
+++ front/src/ui/pages/home.tsx
@@ -1,18 +1,6 @@
 import { ReactElement } from 'react';
-import { Card } from '../components/card';
-import { useTours } from '../hooks/use-tour';
+import { Welcome } from '../components/welcome';
 
 export function Home(): ReactElement {
-  const { tours, isLoading } = useTours();
-
-  if (isLoading || tours.length === 0) return <div>LOADING...</div>;
-
-  return (
-    <div className="card-container">
-      {tours &&
-        tours.map((tour) => {
-          return <Card key={tour.id} cardData={tour} />;
-        })}
-    </div>
-  );
+  return <Welcome />;
 }
