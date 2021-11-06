46e51188da53a4215f6c71e2f5569c8285d4c8dd
CasinoRoyal <lux_ex_tenebris@outlook.com>
2021-11-06 19:23:56 +0200

    Add type for sign-up and sing-in

--- front/src/domains/user.entity.ts
+++ front/src/domains/user.entity.ts
@@ -3,6 +3,16 @@ export type UserName = string;
 export type Email = string;
 export type PhotoUrl = string;
 export type UserRole = 'user' | 'guide' | 'lead-guide' | 'admin';
+export type RegistrationData = {
+  username: UserName;
+  email: Email;
+  password: string;
+  passwordConfirm: string;
+};
+export type AuthenticateData = Omit<
+  RegistrationData,
+  'username' | 'passwordConfirm'
+>;
 
 export class User {
   constructor(
