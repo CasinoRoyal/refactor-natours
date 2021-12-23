23e1825969a92de228414e11eaeaa20e4204223b
CasinoRoyal <lux_ex_tenebris@outlook.com>
2021-12-23 15:22:39 +0200

    - Add httpOnly property  - Delete password from user response  - Add development and production env to front

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -13,19 +13,19 @@ const generateToken = (id) => {
 
 const createAuthToken = (user, statusCode, req, res) => {
   const token = generateToken(user._id);
-  // const cookieOptions = {
-  //   expires: new Date(
-  //     Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-  //   ),
-  //   httpOnly: true,
-  //   secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
-  // };
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: true
+  };
   
-  res.cookie('jwt', token);
+  res.cookie('jwt', token, cookieOptions);
 
   return res.status(statusCode).json({
     status: 'success',
-    token,
     data: {
       user
     }
@@ -58,6 +58,8 @@ exports.login = catchAsyncError(async (req, res, next) => {
     return next(new AppError('Wrong email and/or password', 401));
   };
 
+  delete user._doc.password;
+  
   createAuthToken(user, 200, req, res);
 });
 
@@ -173,7 +175,7 @@ exports.restrictTo = (...roles) => {
 };
 
 exports.forgotPassword = catchAsyncError(async (req, res, next) => {
-  const user = await User.findOne({email: req.body.email});
+  const user = await User.findOne({email: req.body.email}, {password: 0, role: 0});
   if (!user) {
     next(new AppError('User with that email address not found', 404));
   };
